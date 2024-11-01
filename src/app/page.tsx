763792102863d77742a460098cab27d29eaeecf2
Rohit Verma <28vrohit@gmail.com>
2024-11-01 16:14:35 +0530

    add right panel compo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import LeftPanel from "@/components/home/left-panel";
+import RightPanel from "@/components/home/right-panel";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <div className="flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
         <div className="fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30" />
         <LeftPanel />
-        {/* <RightPanel /> */}
+        <RightPanel />
       </div>
     </main>
   );
