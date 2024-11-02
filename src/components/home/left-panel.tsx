418ddc2391d8f02fb8e4b62dae9fe185e17cb360
Rohit Verma <28vrohit@gmail.com>
2024-11-02 13:59:12 +0530

    add user list

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -10,6 +10,7 @@ import ThemeSwitch from "./theme-switch";
 import { conversations } from "@/dummy-data/db";
 import Conversation from "./conversation";
 import { UserButton } from "@clerk/nextjs";
+import UserListDialog from "./user-list-dialog";
 
 const LeftPanel = () => {
   return (
@@ -20,7 +21,7 @@ const LeftPanel = () => {
           <UserButton />
 
           <div className="flex items-center gap-3">
-            <MessageSquareDiff size={20} />{" "}
+            <UserListDialog />
             {/* TODO: This line will be replaced with <UserListDialog /> */}
             <ThemeSwitch />
           </div>
