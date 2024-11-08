dd437111f8175f76b55b31b27d2188d5dba42d58
Rohit Verma <28vrohit@gmail.com>
2024-11-08 21:53:54 +0530

    update list

--- src/components/home/user-list-dialog.tsx
+++ src/components/home/user-list-dialog.tsx
@@ -19,6 +19,7 @@ import { Id } from "../../../convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import toast from "react-hot-toast";
+import { useConversationStore } from "@/store/chat-store";
 
 const UserListDialog = () => {
   const [selectedUsers, setSelectedUsers] = useState<Id<"users">[]>([]);
@@ -35,7 +36,7 @@ const UserListDialog = () => {
   const me = useQuery(api.users.getMe);
   const users = useQuery(api.users.getUsers);
 
-  const setSelectedConversation = null;
+  const { setSelectedConversation } = useConversationStore();
 
   const closeUserListDialog = () => {
     dialogCloseRef.current?.click();
@@ -85,6 +86,17 @@ const UserListDialog = () => {
       const conversationName = isGroup
         ? groupName
         : users?.find((user) => user._id === selectedUsers[0])?.name;
+
+      setSelectedConversation({
+        _id: conversationId,
+        participants: selectedUsers,
+        isGroup,
+        image: isGroup
+          ? renderedImage
+          : users?.find((user) => user._id === selectedUsers[0])?.image,
+        name: conversationName,
+        admin: me?._id!,
+      });
     } catch (err) {
       console.error(err);
       toast.error("Failed to create converstion");
